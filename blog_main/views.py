59c1703f41b9ba4cdf1ad332672398e2fc33555b
repo-Demo <iveerannadesai@gmail.com>
2025-12-09 22:55:25 +0530

    added login and register pages

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
+from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
 from django.http import HttpResponse
 from assignments.models import About
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     categories = Category.objects.all()
@@ -16,5 +19,41 @@ def home(request):
     }
     return render(request,'home.html',context)
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            form.errors
+    else:
+        form = RegistrationForm()
+        
+        
+    form = RegistrationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request,'register.html',context)
 
-    
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(request,username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
